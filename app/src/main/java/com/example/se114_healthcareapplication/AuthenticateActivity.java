23aacfe9044f2f8b312aad1dfcf0bf38cba1f6e0
hiepc <ch.dhiep@gmail.com>
2022-05-10 11:16:08 +0700

    Commit

---
+++ app/src/main/java/com/example/se114_healthcareapplication/AuthenticateActivity.java
@@ -0,0 +1,13 @@
+package com.example.se114_healthcareapplication;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthenticateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticate);
+    }
+}
