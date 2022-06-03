f6d6d2d7b4b32ce8561925f6b5775c2bd42ff0a9
unknown <nhtdforwork@gmail.com>
2022-06-03 16:12:40 +0700

    Update manage data rules

--- app/src/main/java/com/example/se114_healthcareapplication/manage_data.java
+++ app/src/main/java/com/example/se114_healthcareapplication/manage_data.java
@@ -3,6 +3,8 @@ package com.example.se114_healthcareapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.fragment.app.Fragment;
@@ -33,6 +35,8 @@ public class manage_data extends Fragment implements IView<UserPresenter> {
     private EditText heighttxt, weighttxt, firstnametxt, lastnametxt, agetxt;
     private Button acceptbtn;
     private UserPresenter mainPresenter;
+    private UserEntity user;
+    private StatisticEntity stat;
 
     public manage_data() {
         // Required empty public constructor
@@ -77,21 +81,141 @@ public class manage_data extends Fragment implements IView<UserPresenter> {
         agetxt = v.findViewById(R.id.age_edt);
         acceptbtn = v.findViewById(R.id.btn_accept);
         setMainPresenter(new UserPresenter(this));
+        acceptbtn.setEnabled(false);
+        acceptbtn.setBackground(getResources().getDrawable(R.drawable.btn_disabled));
+
+        heighttxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkIfCanUpdate();
+            }
+        });
+
+        weighttxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkIfCanUpdate();
+            }
+        });
+
+        firstnametxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkIfCanUpdate();
+            }
+        });
+
+        lastnametxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkIfCanUpdate();
+            }
+        });
+
+        agetxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkIfCanUpdate();
+            }
+        });
         return v;
     }
 
     @Override
     public void UpdateView(int code, Object entity) {
         if(code == UserPresenter.UPDATE_USER_INFO){
-            UserEntity user = (UserEntity)entity;
+            user = (UserEntity)entity;
             agetxt.setText(String.valueOf(user.Age));
             firstnametxt.setText(user.FirstName);
             lastnametxt.setText(user.LastName);
         }
         if(code == UserPresenter.UPDATE_STATISTIC){
-            StatisticEntity stat = (StatisticEntity)entity;
+            stat = (StatisticEntity)entity;
             heighttxt.setText(String.valueOf(stat.Height));
             weighttxt.setText(String.valueOf(stat.Weight));
+        }
+    }
+
+    private void checkIfCanUpdate(){
+        if(stat!=null && user!=null) {
+
+            if (!heighttxt.getText().toString().isEmpty()
+                    && !weighttxt.getText().toString().isEmpty()
+                    && !lastnametxt.getText().toString().isEmpty()
+                    && !firstnametxt.getText().toString().isEmpty()
+                    && !agetxt.getText().toString().isEmpty()) {
+                double hei = Double.parseDouble(heighttxt.getText().toString());
+                double wei = Double.parseDouble(weighttxt.getText().toString());
+                String lname = lastnametxt.getText().toString();
+                String finame = firstnametxt.getText().toString();
+                int age = Integer.parseInt(agetxt.getText().toString());
+                if (
+                        hei != stat.Height
+                                || wei != stat.Weight
+                                || !lname.equals(user.LastName)
+                                || !finame.equals(user.FirstName)
+                                || age != user.Age
+                ) {
+                    acceptbtn.setEnabled(true);
+                    acceptbtn.setBackground(getResources().getDrawable(R.drawable.btn_intro));
+                } else {
+                    acceptbtn.setEnabled(false);
+                    acceptbtn.setBackground(getResources().getDrawable(R.drawable.btn_disabled));
+                }
+            } else {
+                acceptbtn.setEnabled(false);
+                acceptbtn.setBackground(getResources().getDrawable(R.drawable.btn_disabled));
+            }
         }
     }
 
